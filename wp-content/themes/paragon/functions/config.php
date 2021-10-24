af3117ffe91db8ebed83bc2015a9e3121a87d776
Anatoliy Komarov <admin@komaroff.biz>
2021-10-24 07:30:00 +0300

    Create config.php

---
+++ wp-content/themes/paragon/functions/config.php
@@ -0,0 +1,27 @@
+<?php
+// config
+define('HOME', 'Главная');
+
+$property_status = [
+  '1' => 'Active',
+  '2' => 'Pending',  
+];
+
+$property_type = [
+  '1' => 'Multi-family',
+  '2' => 'Land',
+  '3' => 'Office',
+  '4' => 'Retail',
+  '5' => 'Industrial',
+];
+
+if ( is_user_logged_in() ) {
+    show_admin_bar(true);
+}
+$keyAPI = 'KEY';
+//
+
+function show_map(){
+  global  $keyAPI;
+    echo '<script  src="https://maps.googleapis.com/maps/api/js?key='.$keyAPI.'&callback=initMap&v=weekly"  async></script>';
+}
